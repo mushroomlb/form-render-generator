e4d7c2673baba4219550cdc178d48bec9de550ac
mushroomlb <mushroom_lb@msn.com>
2020-05-02 09:39:57 +0800

    add new method test

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,12 +1,1 @@
-import React from 'react';
-interface SecurityLayoutProps {
-    loading?: boolean;
-}
-interface SecurityLayoutState {
-    isReady: boolean;
-}
-export default class SecurityLayout extends React.Component<SecurityLayoutProps, SecurityLayoutState> {
-    state: SecurityLayoutState;
-    render(): number;
-}
-export {};
+export declare const Hello: (ts: string) => string;
--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,3 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
-class SecurityLayout extends react_1.default.Component {
-    constructor() {
-        super(...arguments);
-        this.state = {
-            isReady: false,
-        };
-    }
-    render() {
-        return (123);
-    }
-}
-exports.default = SecurityLayout;
+exports.Hello = (ts) => 'hello' + ts;
